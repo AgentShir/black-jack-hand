775377d21decd3b6dc1a69c6d426cffa9b265bbc
Shireen Garcia <garciasal@gmail.com>
2017-07-13 16:04:36 -0700

    Thanks for unflagging my account

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,34 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
-let hand = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+//let hand = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]
 
 function handValue (hand) {
+
+    let hand = 
+    //hand = 1 - 10, if face cards = 10, if Ace = 1 or 11
+    //handValue must be less than or equal to 21
+    //how do i cycle through the hand array the first time?
+    //how do i push A to its own array?
+    //
+
+
+    // function arrayCombos(){
+    //     // YOUR CODE HERE
+    //
+    //     let allCombos = []
+    //
+    //     for (let i=0; i < singles.length; i++) {
+    //       for (j=0; j < singles.length; j++){
+    //
+    //       let combos =[]
+    //       combos.push(singles[i])
+    //       combos.push(singles[j])
+    //       allCombos.push(combos)
+    //       }
+    //     }
+    //     return allCombos
+    // }
 
 
   return;
