ad50147416dc26d60147656231d0fefb08a5183c
Shireen Garcia <garciasal@gmail.com>
2017-08-13 13:31:20 -0700

    Ready for submission

--- scripts/main.js
+++ scripts/main.js
@@ -8,37 +8,34 @@
    you'll need to parse through that first before you can start to
    write your logic.
 */
-//let hand = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]
+let hand = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" ]
 
-function handValue (hand) {
+function handValue(hand) {
+    //1. Loop over the hand array and calculate the value of each card
+    let handValue = 0;
 
-    let hand = 
-    //hand = 1 - 10, if face cards = 10, if Ace = 1 or 11
-    //handValue must be less than or equal to 21
-    //how do i cycle through the hand array the first time?
-    //how do i push A to its own array?
-    //
+    for(let i = 0; i < hand.length; i++){
+      if (hand[i] === "J" || hand[i] === "Q" || hand [i] === "K"){
+        hand[i] = "10";
+      } else if (hand[i] === "A"){
+        if (handValue > "10"){
+          hand[i] = "1";
+        } else {
+          hand[i] = "11";
+        }
+      }
 
+    handValue = parseInt(hand[i]) + handValue;
+      if (handValue > 21) {
+        (handValue -= 10);
+        }
+      }
+    //2. if the card is a K, Q, J then the value is 10
+    //3. if the card is A then add 11
+    //4. else just use the face value of the card to add to total
+    //5. If total is over 21, start changing A values to 1
 
-    // function arrayCombos(){
-    //     // YOUR CODE HERE
-    //
-    //     let allCombos = []
-    //
-    //     for (let i=0; i < singles.length; i++) {
-    //       for (j=0; j < singles.length; j++){
-    //
-    //       let combos =[]
-    //       combos.push(singles[i])
-    //       combos.push(singles[j])
-    //       allCombos.push(combos)
-    //       }
-    //     }
-    //     return allCombos
-    // }
-
-
-  return;
+  return handValue;
 }
 
 
